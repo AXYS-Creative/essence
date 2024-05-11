ade3f945a5cdb736c4262db3d3362deea5fbc275
Aaron Garcia <axyscreativeagency@gmail.com>
2024-05-10 22:30:37 -0600

    cta placement progress

--- src/javascript/scroll.js
+++ src/javascript/scroll.js
@@ -1,5 +1,14 @@
 import { maxXxl, maxXl, maxLg, maxMd } from "./utility.js";
 
+const siteHeader = document.querySelector(".site-header");
+const menuBtn = document.querySelector(".menu-btn");
+const ctaWrapper = document.querySelector(".cta-wrapper");
+
+const headerLinks = document.querySelectorAll(".header-links__link");
+
+// Header Defaults
+menuBtn.setAttribute("tabindex", "-1");
+
 const throttle = (func, limit) => {
   let lastFunc;
   let lastRan;
@@ -21,57 +30,31 @@ const throttle = (func, limit) => {
   };
 };
 
-const siteHeader = document.querySelector(".site-header"),
-  menuBtn = document.querySelector(".menu-btn"),
-  ctaWrapper = document.querySelector(".cta-wrapper");
+const updateScrollDependentElements = (scrollPosition) => {
+  const scrollHeight = document.documentElement.scrollHeight;
+  const clientHeight = document.documentElement.clientHeight;
 
-const headerLinks = document.querySelectorAll(".header-links__link");
-
-// Header Defaults
-menuBtn.setAttribute("tabindex", "-1");
-
-//
-// All Scroll Animations that require SCRUBBING
-//
-
-const checkScroll = () => {
-  const scrollPosition = window.scrollY;
-
-  // Detect if the user has scrolled away from the top of the page
+  // Manage header
   if (scrollPosition >= 24) {
     siteHeader.classList.add("scroll-active");
-    // ctaWrapper.classList.add("scroll-active");
-
     menuBtn.setAttribute("aria-hidden", "false");
     menuBtn.removeAttribute("tabindex");
-
-    headerLinks.forEach((link) => link.setAttribute("tabindex", -1));
+    headerLinks.forEach((link) => link.setAttribute("tabindex", "-1"));
   } else {
     siteHeader.classList.remove("scroll-active");
-    // ctaWrapper.classList.remove("scroll-active");
-
     headerLinks.forEach((link) => link.removeAttribute("tabindex"));
-
     menuBtn.setAttribute("aria-hidden", "true");
     menuBtn.setAttribute("tabindex", "-1");
   }
 
-  // if (scrollPosition >= 196) {
-  if (scrollPosition >= 480) {
-    ctaWrapper.classList.add("scroll-active");
-  } else {
-    ctaWrapper.classList.remove("scroll-active");
-  }
-
-  // Detect if the user is near the bottom of the page
-  const scrollHeight = document.documentElement.scrollHeight;
-  const clientHeight = document.documentElement.clientHeight;
-
+  // Adjust header at bottom of page
   const handleScrollBottom = (threshold) => {
     if (scrollHeight - (scrollPosition + clientHeight) < threshold) {
       siteHeader.classList.add("header-scroll-bottom");
+      menuBtn.setAttribute("tabindex", "-1");
     } else {
       siteHeader.classList.remove("header-scroll-bottom");
+      menuBtn.setAttribute("tabindex", "0");
     }
   };
 
@@ -84,6 +67,76 @@ const checkScroll = () => {
   } else {
     handleScrollBottom(480);
   }
+
+  //
+  // CTA
+  //
+  const rootElem = document.documentElement;
+
+  let ctaScrollTrigger; // Distance from the top (in px) needed to scroll to reposition the CTA
+  if (maxLg.matches) {
+    ctaScrollTrigger = 424;
+  } else if (maxXl.matches) {
+    ctaScrollTrigger = 296;
+  } else if (maxXxl.matches) {
+    ctaScrollTrigger = 200;
+  } else {
+    ctaScrollTrigger = 480;
+  }
+
+  let ctaPosition; // CTA placement in the hero
+  if (maxLg.matches) {
+    ctaPosition = 424;
+  } else if (maxXl.matches) {
+    ctaPosition = 296;
+  } else if (maxXxl.matches) {
+    ctaPosition = 548;
+    rootElem.style.setProperty("--cta-position", `${ctaPosition}px`);
+  } else {
+    ctaPosition = 640;
+  }
+
+  // Update CTA animation based on scroll position
+  if (scrollPosition >= ctaScrollTrigger) {
+    ctaWrapper.classList.add("scroll-active");
+    ctaWrapper.style.animationName = "cta-animated";
+  } else {
+    ctaWrapper.classList.remove("scroll-active");
+    ctaWrapper.style.animationName = "cta-default";
+  }
+
+  console.log(ctaPosition, ctaScrollTrigger);
+
+  ctaStylesheet.innerHTML = `
+  @keyframes cta-animated {
+    from {
+      top: calc(${ctaPosition}px - ${ctaScrollTrigger}px);
+    }
+    to {
+      top: calc(100vh - 64px * 1.5);
+    }
+  }
+
+  @keyframes cta-default {
+    from {
+      top: calc(${scrollPosition + clientHeight}px - 64px * 1.5);
+    }
+    to {
+      top: ${ctaPosition}px;
+    }
+  }`;
 };
 
-window.addEventListener("scroll", throttle(checkScroll, 50)); // Throttle checkScroll, adjust 100ms as needed
+// CTA Reposition
+// let ctaScrollTrigger = 480; // Distance from the top (in px) needed to scroll to reposition the CTA
+let ctaStylesheet = document.createElement("style");
+document.head.appendChild(ctaStylesheet);
+
+// Event listener for scroll events
+window.addEventListener(
+  "scroll",
+  throttle(() => {
+    const scrollPosition = window.scrollY;
+    updateScrollDependentElements(scrollPosition);
+  }, 50)
+);
