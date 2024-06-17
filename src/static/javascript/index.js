c4bbb3ce6ddfbaeb8de078341434e2fad12d94b7
Aaron Garcia <axyscreativeagency@gmail.com>
2024-06-17 06:32:32 -0600

    magnet affect, cta fade in

--- src/static/javascript/index.js
+++ src/static/javascript/index.js
@@ -8,5 +8,6 @@ import "./components/posts.js";
 import "./core/animations.js";
 // import "./core/lenis.js";
 import "./core/loading.js";
+import "./core/magnet.js";
 import "./core/mouseCursor.js";
 import "./core/newsletter.js";
