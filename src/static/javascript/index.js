d34e7aef6cf05a08ff57c40d89acfccfc0e051b6
Aaron Garcia <axyscreativeagency@gmail.com>
2024-06-27 07:58:39 -0600

    benchmark for threejs, removed curtains

--- src/static/javascript/index.js
+++ src/static/javascript/index.js
@@ -6,9 +6,9 @@ import "./components/header.js";
 import "./components/posts.js";
 
 import "./core/animations.js";
-import "./core/curtains.js";
 // import "./core/lenis.js";
 import "./core/loading.js";
 import "./core/magnet.js";
 import "./core/mouseCursor.js";
 import "./core/newsletter.js";
+import "./core/three.js";
