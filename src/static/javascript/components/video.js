3f0b79807e9fd5cca3d15dccebc362ddbdee2345
Aaron Garcia <axyscreativeagency@gmail.com>
2024-07-15 19:25:37 -0600

    video section progress - video cursor / section-theme mixin

--- src/static/javascript/components/video.js
+++ src/static/javascript/components/video.js
@@ -1,19 +1,40 @@
-const videoSection = document.querySelector(".video-section");
-const iframe = document.querySelector(".video-section iframe");
-const videoBtn = document.querySelector(".video-section__controls-play-btn");
+const videoSection = document.querySelector(".video-section"),
+  videoPoster = document.querySelector(".video-section__poster"),
+  iframe = document.querySelector(".video-section iframe"),
+  videoBtn = document.querySelector(".video-section__play-btn");
 
 if (videoSection) {
+  iframe.classList.add("podcast-iframe");
+
   const playVideo = () => {
     iframe.classList.add("active");
   };
 
   const closeVideo = (e) => {
-    if (e.target === e.currentTarget) {
+    if (!e.target.classList.contains("podcast-iframe")) {
       iframe.classList.remove("active");
     }
   };
 
-  videoBtn.addEventListener("click", playVideo);
+  videoPoster.addEventListener("click", playVideo);
 
   videoSection.addEventListener("click", closeVideo);
+
+  // Play button / Mouse cursor
+  if (window.matchMedia("(pointer: fine)").matches) {
+    videoPoster.addEventListener("mousemove", (e) => {
+      const rect = videoPoster.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+      const posterWidth = videoPoster.offsetWidth / 2;
+      const posterHeight = videoPoster.offsetHeight / 2;
+      videoBtn.style.translate = `calc(${x - posterWidth}px - 50%) calc(${
+        y - posterHeight
+      }px - 50%)`;
+    });
+
+    videoPoster.addEventListener("mouseleave", () => {
+      videoBtn.style.translate = `-50% -50%`;
+    });
+  }
 }
