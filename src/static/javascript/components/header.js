28a8ef29c922380501dad5be804178d4805cb19f
Aaron Garcia <axyscreativeagency@gmail.com>
2024-07-31 17:16:17 -0600

    rename heading link underline

--- src/static/javascript/components/header.js
+++ src/static/javascript/components/header.js
@@ -1,7 +1,7 @@
 import { path, pageClasses } from "../utility.js";
 
 const headerLinks = document.querySelector(".header-links"),
-  headerLinksUnderline = document.querySelector(
+  headerLinksIndicator = document.querySelector(
     ".header-links__hover-indicator"
   ),
   linkItems = headerLinks.querySelectorAll("li"),
@@ -12,25 +12,25 @@ const moveUnderlineToLink = (link) => {
   const parentRect = headerLinks.getBoundingClientRect();
   const leftPosition = linkRect.left - parentRect.left;
 
-  headerLinksUnderline.style.left = `${leftPosition}px`;
-  headerLinksUnderline.style.width = `${linkRect.width}px`;
+  headerLinksIndicator.style.left = `${leftPosition}px`;
+  headerLinksIndicator.style.width = `${linkRect.width}px`;
 };
 
 linkItems.forEach((link) => {
   link.addEventListener("mouseover", () => {
     moveUnderlineToLink(link);
-    headerLinksUnderline.classList.add("show-indicator");
+    headerLinksIndicator.classList.add("show-indicator");
   });
   link.addEventListener("focusin", () => {
     moveUnderlineToLink(link);
-    headerLinksUnderline.classList.add("show-indicator");
+    headerLinksIndicator.classList.add("show-indicator");
   });
 
   link.addEventListener("mouseleave", () => {
-    headerLinksUnderline.classList.remove("show-indicator");
+    headerLinksIndicator.classList.remove("show-indicator");
   });
   link.addEventListener("focusout", () => {
-    headerLinksUnderline.classList.remove("show-indicator");
+    headerLinksIndicator.classList.remove("show-indicator");
   });
 });
 
